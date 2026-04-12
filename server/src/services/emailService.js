8c9a283780c8ab3cbf1dfb7b78bdd4888d8549c6
omjaisatya <126938261+omjaisatya@users.noreply.github.com>
2026-04-12 13:48:41 +0530

    testing nodemailer: getting an error while sending mail

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -1,10 +1,10 @@
 import nodemailer from "nodemailer";
-import { EMAIL_PASS, EMAIL_SERVICE, EMAIL_USE } from "../config/env.config.js";
+import { EMAIL_PASS, EMAIL_USE } from "../config/env.config.js";
 import dns from "dns";
 dns.setDefaultResultOrder("ipv4first");
 
 const mailTransporter = nodemailer.createTransport({
-  service: EMAIL_SERVICE,
+  service: "Gmail",
   auth: {
     user: EMAIL_USE,
     pass: EMAIL_PASS,
