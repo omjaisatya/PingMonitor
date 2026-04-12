4fd2b57d8e7bc075cf1d878ea6e52af040f7c0fa
omjaisatya <126938261+omjaisatya@users.noreply.github.com>
2026-04-12 12:40:37 +0530

    add env for deploy test

--- server/src/config/env.config.js
+++ server/src/config/env.config.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 
 if (process.env.NODE_ENV === "production") {
   dotenv.config({ path: ".env.production" });
+} else if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
 } else {
   dotenv.config();
 }
